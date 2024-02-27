d6965f5be6d57f3c9d17dcdd3841ed925d718907
jason <whdgur826333@gmail.com>
2024-02-26 18:15:04 -0700

    LogIn finished

---
+++ server/src/test/java/serviceTests/LogInTest.java
@@ -0,0 +1,39 @@
+package serviceTests;
+
+import chess.ChessGame;
+import dataAccess.DataAccessException;
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryGameDAO;
+import dataAccess.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import service.ClearService;
+import service.LogInRequest;
+import service.LogInResult;
+import service.LogInService;
+
+public class LogInTest {
+    private static LogInService logInService;
+    static MemoryUserDAO memoryUserDAO = new MemoryUserDAO();
+    static MemoryAuthDAO memoryAuthDAO = new MemoryAuthDAO();
+    static MemoryGameDAO memoryGameDAO = new MemoryGameDAO();
+
+    @BeforeAll
+    public static void createLogInService() {
+        logInService = new LogInService(memoryAuthDAO,memoryUserDAO);
+
+        memoryUserDAO.createUser(new UserData("test1","1234","test@gmail.com"));
+
+    }
+    @Test
+    public void testLogIn() throws DataAccessException{
+        LogInResult actual = logInService.login(new LogInRequest("test1","1234"));
+        LogInResult expected  = new LogInResult("test1","1234");
+        Assertions.assertEquals(expected.username(),actual.username());
+//        Assertions.assertThrows()
+    }
+}

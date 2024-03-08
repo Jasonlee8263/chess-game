97ead12960f4f7758782e08c9cf11a3f540f5d88
jason <whdgur826333@gmail.com>
2024-03-07 18:10:37 -0700

    test cases for auth insert

--- server/src/test/java/dataAccessTests/GameDAOTest.java
+++ server/src/test/java/dataAccessTests/GameDAOTest.java
@@ -1,4 +1,56 @@
 package dataAccessTests;
 
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import dataAccess.MySqlGameDAO;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GameDAOTest {
+    private static GameDAO gameDAO;
+    @BeforeAll
+    public static void createGameDAO() throws DataAccessException {
+        gameDAO = new MySqlGameDAO();
+    }
+    @BeforeEach
+    public void clear() throws DataAccessException {
+        gameDAO.clear();
+    }
+    @Test
+    public void testCreateGame(){
+
+    }
+    @Test
+    public void testCreateGameFail(){
+
+    }
+    @Test
+    public void testGetGame(){
+
+    }
+    @Test
+    public void testGetGameFail(){
+
+    }
+    @Test
+    public void testListGame(){
+
+    }
+    @Test
+    public void testListGameFail(){
+
+    }
+    @Test
+    public void testUpdateGame(){
+
+    }
+    @Test
+    public void testUpdateGameFail(){
+
+    }
+    @Test
+    public void testClear(){
+
+    }
 }
--- server/src/test/java/dataAccessTests/UserDAOTest.java
+++ server/src/test/java/dataAccessTests/UserDAOTest.java
@@ -1,4 +1,40 @@
 package dataAccessTests;
 
+import dataAccess.DataAccessException;
+import dataAccess.MySqlUserDAO;
+import dataAccess.UserDAO;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class UserDAOTest {
+    private static UserDAO userDAO;
+    @BeforeAll
+    public static void createUserDAO() throws DataAccessException {
+        userDAO = new MySqlUserDAO();
+    }
+    @BeforeEach
+    public void clear() throws DataAccessException {
+        userDAO.clear();
+    }
+    @Test
+    public void testCreateUser(){
+
+    }
+    @Test
+    public void testCreateUserFail(){
+
+    }
+    @Test
+    public void testGetUser(){
+
+    }
+    @Test
+    public void testGetUserFail(){
+
+    }
+    @Test
+    public void testClear(){
+
+    }
 }
